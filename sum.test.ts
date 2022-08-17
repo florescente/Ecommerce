9f89583c013804aaceaf538b250669fd2f50ee58
florescente <florescente@gmail.com>
2022-08-17 02:31:29 -0300

    jest with typescript

---
+++ sum.test.ts
@@ -0,0 +1,9 @@
+import { sum } from './sum'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+test('adds number + 0 to equal number', () => {
+  expect(sum(2, 0)).toBe(2)
+})
